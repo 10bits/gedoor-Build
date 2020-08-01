5b12edddbe7b16981230da3989ca2b329fa789c0
10bits <loseher2020@outlook.com>
2020-08-01 22:32:03 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+#解决安装程序共存问题
+sed "s/'.release'/'.releaseA'/" /opt/legado/app/build.gradle -i
+sed 's/.release/.releaseA/g' /opt/legado/app/google-services.json -i 
+
 #设置搜索界面浮动按钮颜色
 sed '98a\        android:backgroundTint="#389099"' /opt/legado/app/src/main/res/layout/activity_book_search.xml -i
 
