f1f744056437cba44121bc6444b5e59e05160041
10bits <loseher2020@outlook.com>
2020-08-01 22:21:04 +0800

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#设置搜索界面浮动按钮颜色
+sed '98a\        android:backgroundTint="#389099"' /opt/legado/app/src/main/res/layout/activity_book_search.xml -i
+
+#缩小apk体积
+#sed 's/minifyEnabled false/minifyEnabled true/g' /opt/legado/app/build.gradle -i
+#sed '59a\            shrinkResources true'       /opt/legado/app/build.gradle -i
