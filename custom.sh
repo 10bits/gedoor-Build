ba6ffd4408c3b6ea0ae1fd98a3e1645b5f43f55f
10bits <loseher2020@outlook.com>
2020-08-02 06:29:52 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -5,8 +5,8 @@ sed "s/'.release'/'.releaseA'/" /opt/legado/app/build.gradle -i
 sed 's/.release/.releaseA/g' /opt/legado/app/google-services.json -i 
 
 #设置搜索界面浮动按钮颜色
-sed '98a\        android:backgroundTint="#389099"' /opt/legado/app/src/main/res/layout/activity_book_search.xml -i
+sed '/id\/fb_stop/a\        android:backgroundTint="#389099"' /opt/legado/app/src/main/res/layout/activity_book_search.xml -i
 
 #缩小apk体积
-#sed 's/minifyEnabled false/minifyEnabled true/g' /opt/legado/app/build.gradle -i
-#sed '59a\            shrinkResources true'       /opt/legado/app/build.gradle -i
+#sed '/minifyEnabled/i\            shrinkResources true' /opt/legado/app/build.gradle -i
+#sed 's/minifyEnabled false/minifyEnabled true/g'        /opt/legado/app/build.gradle -i
