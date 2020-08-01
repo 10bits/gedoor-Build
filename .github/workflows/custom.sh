6ee1c898ef85433c6674a0cec24bb1aabef2d237
10bits <loseher2020@outlook.com>
2020-08-01 21:34:54 +0800

    Update custom.sh

--- .github/workflows/custom.sh
+++ .github/workflows/custom.sh
@@ -3,4 +3,5 @@
 sed '98a\        android:backgroundTint="#389099"' /opt/legado/app/src/main/res/layout/activity_book_search.xml -i
 
 #缩小apk体积
-sed '59a\            shrinkResources true' /opt/legado/app/build.gradle -i
+sed 's/minifyEnabled false/minifyEnabled true/g' /opt/legado/app/build.gradle -i
+sed '59a\            shrinkResources true'       /opt/legado/app/build.gradle -i
