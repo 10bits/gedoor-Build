034eae1dd17c63f0c45879ba194d6b4d5b3357f4
10bits <loseher2020@outlook.com>
2020-08-02 13:28:56 +0800

    Create check_tag.sh

---
+++ check_tag.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#本脚本用来获取clone项目最新tag
+
+git clone https://github.com/gedoor/legado.git /opt/legado
+cd /opt/legado
+LatestCheck = $(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
+LatestTag   = $(git describe --tags `git rev-list --tags --max-count=1`)
+tag = $LatestTag
+echo "检查时间:$LatestCheck"
+echo "最新tag:$LatestTag"
