1f2f62706bee488aadcfd8f00cfcd9f0de025106
10bits <loseher2020@outlook.com>
2020-08-02 13:30:49 +0800

    Update schedule.sh

--- schedule.sh
+++ schedule.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 #本脚本会定时更新README.md里的最新tag显示
 
-git clone https://github.com/gedoor/legado.git /opt/legado
-cd /opt/legado
-LatestCheck = $(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
-LatestTag   = $(git describe --tags `git rev-list --tags --max-count=1`)
-echo "检查时间:$LatestCheck"
-echo "最新tag:$LatestTag"
+source $GITHUB_WORKSPACE/check_tag.sh
 
 cd $GITHUB_WORKSPACE
 sed "5c > 当前最新tag:$LatestTag 上次检查时间:$LatestCheck" README.md -i
