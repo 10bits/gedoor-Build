f266bf0160a00c33de5750d5a38269e72a382676
10bits <loseher2020@outlook.com>
2020-08-02 14:14:13 +0800

    Update check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -3,8 +3,7 @@
 
 git clone https://github.com/gedoor/legado.git /opt/legado
 cd /opt/legado
-LatestCheck = $(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
-LatestTag   = $(git describe --tags `git rev-list --tags --max-count=1`)
-tag = $LatestTag
+LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
+LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "检查时间:$LatestCheck"
 echo "最新tag:$LatestTag"
