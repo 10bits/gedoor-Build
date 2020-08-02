e336b440ebe0e92a4fb93d31159a68d69581b061
10bits <loseher2020@outlook.com>
2020-08-02 11:23:14 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #本脚本用来clone远端仓库
-
-git clone https://github.com/gedoor/legado.git /opt/legado
-cd /opt/legado
+echo {{ env.workspace }}
+git clone https://github.com/gedoor/legado.git {{ env.workspace }}
+cd {{ env.workspace }}
 LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "最新tag:$LatestTag"
 git checkout $LatestTag
 echo "清空18PlusList.txt"
-echo "">/opt/legado/app/src/main/assets/18PlusList.txt
+echo "">{{ env.workspace }}/app/src/main/assets/18PlusList.txt
