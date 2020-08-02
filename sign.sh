b3c92b8a279e234e26e710eb4d549b449b6c3799
10bits <loseher2020@outlook.com>
2020-08-02 11:47:31 +0800

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #本脚本用来给app增加签名
 
-cp $GITHUB_WORKSPACE/.github/workflows/legado.jks /opt/legado/app/legado.jks
-sed '$a\RELEASE_STORE_FILE=./legado.jks'          /opt/legado/gradle.properties -i 
-sed '$a\RELEASE_KEY_ALIAS=legado'                 /opt/legado/gradle.properties -i
-sed '$a\RELEASE_STORE_PASSWORD=gedoor_legado'     /opt/legado/gradle.properties -i
-sed '$a\RELEASE_KEY_PASSWORD=gedoor_legado'       /opt/legado/gradle.properties -i
+cp $GITHUB_WORKSPACE/.github/workflows/legado.jks $workspace/app/legado.jks
+sed '$a\RELEASE_STORE_FILE=./legado.jks'          $workspace/gradle.properties -i 
+sed '$a\RELEASE_KEY_ALIAS=legado'                 $workspace/gradle.properties -i
+sed '$a\RELEASE_STORE_PASSWORD=gedoor_legado'     $workspace/gradle.properties -i
+sed '$a\RELEASE_KEY_PASSWORD=gedoor_legado'       $workspace/gradle.properties -i
