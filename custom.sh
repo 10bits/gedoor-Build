37caad412def7bc11adbe2839873e842cb9c9892
10bits <loseher2020@outlook.com>
2020-08-03 17:30:49 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,7 +3,7 @@
 
 #解决安装程序共存问题
 sed "s/'.release'/'.releaseA'/" $workspace/app/build.gradle -i
-sed 's/.release/.releaseA/g'    $workspace/app/google-services.json -i 
+sed 's/.release/.releaseA/'    $workspace/app/google-services.json -i 
 
 #设置搜索界面浮动按钮颜色
 sed '/id\/fb_stop/a\        android:backgroundTint="#389099"' $workspace/app/src/main/res/layout/activity_book_search.xml -i
