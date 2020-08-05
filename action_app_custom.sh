622f2c2d07b503d60438fabe11312822d3a6f9a7
10bits <loseher2020@outlook.com>
2020-08-05 20:47:36 +0800

    update

--- action_app_custom.sh
+++ action_app_custom.sh
@@ -3,7 +3,6 @@
 
 function app_clear_18plus()
 {
-    echo "当前app:$APP_NAME"
     if [ $APP_NAME = 'legado' ]; then
         echo "清空18PlusList.txt"
         echo "">$APP_WORKSPACE/app/src/main/assets/18PlusList.txt
@@ -18,7 +17,6 @@ function app_color_set()
 }
 function app_resources_unuse()
 {
-    #删除一些用不到的资源
     if [ $APP_NAME = 'legado' ]; then
         echo "删除一些用不到的资源"
         rm $APP_WORKSPACE/app/src/main/assets/bg      -rf
@@ -32,17 +30,19 @@ function app_minify()
     sed 's/minifyEnabled false/minifyEnabled true/'         $APP_WORKSPACE/app/build.gradle -i
 }
 
-function app_live_together(){
+function app_live_together()
+{
     #解决安装程序共存问题
     sed "s/'.release'/'.releaseA'/" $APP_WORKSPACE/app/build.gradle -i
     sed 's/.release/.releaseA/'     $APP_WORKSPACE/app/google-services.json -i 
 }
-function app_sign(){
+function app_sign()
+{
+    echo "给apk增加签名"
     cp $GITHUB_WORKSPACE/.github/workflows/legado.jks $APP_WORKSPACE/app/legado.jks
     sed '$a\RELEASE_STORE_FILE=./legado.jks'          $APP_WORKSPACE/gradle.properties -i 
     sed '$a\RELEASE_KEY_ALIAS=legado'                 $APP_WORKSPACE/gradle.properties -i
     sed '$a\RELEASE_STORE_PASSWORD=gedoor_legado'     $APP_WORKSPACE/gradle.properties -i
     sed '$a\RELEASE_KEY_PASSWORD=gedoor_legado'       $APP_WORKSPACE/gradle.properties -i
-
 }
 app_clear_18plus;app_sign;app_live_together;app_minify;app_resources_unuse
