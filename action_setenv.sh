6c5a66ed80bce200cc43791f34e9dd0403c92376
10bits <loseher2020@outlook.com>
2020-08-05 09:53:15 +0800

    update

--- action_setenv.sh
+++ action_setenv.sh
@@ -8,8 +8,8 @@ APP_WORKSPACE="/opt/$APP_NAME"
 APP_UPLOAD_NAME="$APP_NAME apk"
 APP_UPLOAD="$APP_WORKSPACE/app/build/outputs/apk/app/release"
 
-set_env APP_NAME        $APP_NAME
-set_ENV APP_WORKSPACE   $APP_WORKSPACE
+set_env APP_NAME  e      $APP_NAME
+set_env APP_WORKSPACE   $APP_WORKSPACE
 set_env APP_GIT_URL     $APP_GIT_URL
 set_env APP_UPLOAD_NAME $APP_UPLOAD_NAME
 set_env APP_UPLOAD      $APP_UPLOAD
