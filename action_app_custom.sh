3e7d1a65bd33e806d33157cd830422488378d780
10bits <loseher2020@outlook.com>
2020-08-06 22:09:38 +0800

    update

--- action_app_custom.sh
+++ action_app_custom.sh
@@ -59,6 +59,7 @@ function app_other()
         echo "$APP_NAME 解压MyBookshelf_Keys.zip"
         unzip -o $APP_WORKSPACE/app/MyBookshelf_Keys.zip -d $APP_WORKSPACE/app
         rm $APP_WORKSPACE/app/gradle.properties
+        sed 's/com.gedoor.monkeybook/com.kunfei.bookshelf/' $APP_WORKSPACE/app/build.gradle -i
     fi
 }
 app_clear_18plus;app_sign;app_live_together;app_other
