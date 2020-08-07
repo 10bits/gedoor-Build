74fe4767a9a75ed2e273f1a8de260203aa1a3a83
10bits <loseher2020@outlook.com>
2020-08-07 21:29:36 +0800

    update

--- action_clone.sh
+++ action_clone.sh
@@ -7,6 +7,9 @@ function init_workspace()
     git clone $APP_GIT_URL $APP_WORKSPACE
     cd $APP_WORKSPACE
     LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+    if [ -n "$SECRETS_TAG" ]; then
+        LatestTag=$SECRETS_TAG
+    fi
     git checkout $LatestTag
     echo ::set-env name=APP_LATEST_TAG::$LatestTag
     echo ::set-env name=APP_UPLOAD_NAME::$APP_NAME-$LatestTag
