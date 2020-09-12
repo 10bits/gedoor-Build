3ffcbfc253e948b3ab2adc337ff4707cee9552c6
10bits <loseher2020@outlook.com>
2020-09-12 14:16:24 +0800

    保存最新版本信息

--- action_schedule.sh
+++ action_schedule.sh
@@ -3,8 +3,11 @@
 
 LatestTag=$APP_LATEST_TAG
 LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
+
 cd $GITHUB_WORKSPACE
 sed "5c > 当前最新tag:$LatestTag 上次检查时间:$LatestCheck" README.md -i
+sed "1i$APP_LATEST_TAG" $APP_WORKSPACE/.github/workflows/.lastcheck
+
 git config user.name  github-actions
 git config user.email github-actions@github.com
 git commit -a -m "update README.md"
