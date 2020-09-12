445dcb1a5c25ec33b1fdf3e6083c8af34633aab7
10bits <loseher2020@outlook.com>
2020-09-13 00:01:45 +0800

    Update action_schedule.sh

--- action_schedule.sh
+++ action_schedule.sh
@@ -22,7 +22,7 @@ cd $APP_WORKSPACE
 LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
 set_env APP_LATEST_TAG  $LatestTag
-set_env APP_UPLOAD_NAME $APP_NAME_$LatestTag
+set_env APP_UPLOAD_NAME "$APP_NAME_$LatestTag"
 
 cd $GITHUB_WORKSPACE
 sed "5c > 当前最新tag:$LatestTag 上次检查时间:$LatestCheck" README.md -i
