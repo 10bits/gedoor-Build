9d4f9ae4554ee9bb34633fac3846f90e6e51fa14
10bits <loseher2020@outlook.com>
2020-09-12 14:52:32 +0800

    Update action_schedule.sh

--- action_schedule.sh
+++ action_schedule.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 #本脚本会定时更新README.md里的最新tag显示
-if [ $APP_NAME = 'legado' ]; then
-  cd $GITHUB_WORKSPACE
-  LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
-  sed "5c > 当前最新tag:$APP_LATEST_TAG 上次检查时间:$LatestCheck" README.md -i
-  sed "1i $APP_LATEST_TAG" $APP_WORKSPACE/.github/workflows/.lastcheck -i
-  git config user.name  github-actions
-  git config user.email github-actions@github.com
-  git commit -a -m "update README.md"
-  git push
-fi
+
+git clone https://github.com/gedoor/legado.git /opt/legado
+cd /opt/legado
+LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
+
+cd $GITHUB_WORKSPACE
+sed "5c > 当前最新tag:$LatestTag 上次检查时间:$LatestCheck" README.md -i
+sed "1i $LatestTag" .github/workflows/.lastcheck -i
+git config user.name  github-actions
+git config user.email github-actions@github.com
+git commit -a -m "update README.md"
+git push
