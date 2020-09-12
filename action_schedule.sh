e0c0c38b6df9fbed9272ed03122ba66c9951eafd
10bits <loseher2020@outlook.com>
2020-09-12 14:24:26 +0800

    Update action_schedule.sh

--- action_schedule.sh
+++ action_schedule.sh
@@ -4,7 +4,7 @@ if [ $APP_NAME = 'legado' ]; then
   cd $GITHUB_WORKSPACE
   LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
   sed "5c > 当前最新tag:$APP_LATEST_TAG 上次检查时间:$LatestCheck" README.md -i
-  sed "1i$APP_LATEST_TAG" $APP_WORKSPACE/.github/workflows/.lastcheck -i
+  sed "1i $APP_LATEST_TAG" $APP_WORKSPACE/.github/workflows/.lastcheck -i
   git config user.name  github-actions
   git config user.email github-actions@github.com
   git commit -a -m "update README.md"
