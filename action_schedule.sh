2ccab21bdfb8ea2b5948c02047f11e6ede17a609
10bits <loseher2020@outlook.com>
2020-09-20 23:13:38 +0800

    Update action_schedule.sh

--- action_schedule.sh
+++ action_schedule.sh
@@ -33,6 +33,6 @@ if version_gt $LatestTag $(cat .lastcheck|sed -n 1p); then
   sed "1i $LatestTag" .lastcheck -i
   git config user.name  github-actions
   git config user.email github-actions@github.com
-  git commit -a -m "update README.md"
+  git commit -a -m "Release $APP_NAME-$LatestTag"
   git push
 fi
