a1586e33c78233dbf892e8c2a29ed0d2aa189584
10bits <loseher2020@outlook.com>
2020-10-07 19:15:16 +0800

    update

--- action_clone.sh
+++ action_clone.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #本脚本用来clone远端仓库
+
 function set_env() { echo "$1=$2" >> $GITHUB_ENV; }
+
 #建立工作目录
 function init_workspace()
 {
@@ -11,7 +13,6 @@ function init_workspace()
         LatestTag=$SECRETS_TAG
     fi
     git checkout $LatestTag
-    set_env APP_LATEST_TAG  $LatestTag
     set_env APP_UPLOAD_NAME $APP_NAME-$LatestTag
     echo $LatestTag
 }
