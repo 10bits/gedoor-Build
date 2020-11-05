c17f8bcc68cd35bc39225b7e6a8417871c6b9f73
10bits <loseher2020@outlook.com>
2020-11-05 14:17:45 +0800

    update

--- action_schedule.sh
+++ action_schedule.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #本脚本会定时更新README.md里的最新tag显示
 
-function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 function set_env() { echo "$1=$2" >> $GITHUB_ENV; }
 
 APP_NAME="legado"
@@ -23,16 +22,7 @@ cd $APP_WORKSPACE
 LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
 git checkout $LatestTag
-set_env APP_LATEST_TAG  $LatestTag
-set_env APP_UPLOAD_NAME $APP_NAME-$LatestTag
-
-cd $GITHUB_WORKSPACE
-set_env APP_LAST_TAG  $(cat .lastcheck|sed -n 1p)
-if version_gt $LatestTag $(cat .lastcheck|sed -n 1p); then
-  sed "5c > 当前最新tag:$LatestTag 上次检查时间:$LatestCheck" README.md -i
-  sed "1i $LatestTag" .lastcheck -i
-  git config user.name  github-actions
-  git config user.email github-actions@github.com
-  git commit -a -m "$APP_NAME-$LatestTag release"
-  git push
-fi
+set_env APP_LATEST_TAG    $LatestTag
+set_env APP_LATEST_CHECK  $LatestCheck
+set_env APP_UPLOAD_NAME   $APP_NAME-$LatestTag
+set_env APP_LAST_TAG      $(cat $GITHUB_WORKSPACE/.lastcheck|sed -n 1p)
