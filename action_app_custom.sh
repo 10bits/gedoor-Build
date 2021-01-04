3cbfb29d7b7c43878ec7df5d30aa5904e8c46c87
10bits <loseher2020@outlook.com>
2021-01-03 23:01:19 -0600

    update

--- action_app_custom.sh
+++ action_app_custom.sh
@@ -28,9 +28,9 @@ function app_bugme()
 {
     if [ $APP_NAME = 'legado' ] && [ $REPO_OWNER = '10bits' ]; then
         echo "bugme个人优化 for 10bits"
-        sed "/error(it)/i\isLoading = false"            $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt -i
+        sed "/error(it)/i\isLoading = false"          $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt -i
         sed 's/error(it)/error("网络错误!请尝试继续")/'  $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt -i
-        sed "s/30000L/10000L/"                          $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowViewModel.kt -i
+        sed "s/30000L/8000L/"                         $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowViewModel.kt -i
     fi
 }
 
