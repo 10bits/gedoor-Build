bb1762de536df86f70d032b9a73013735da29966
10bits <loseher2020@outlook.com>
2021-01-11 08:21:32 -0600

    update

--- action_upload.sh
+++ action_upload.sh
@@ -3,7 +3,7 @@
 #上传到webdav
 function webdav_upload()
 {
-  if [ $WEBDAV_UPLOAD = 'true' ] [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
+  if [ $WEBDAV_UPLOAD = 'true' ] && [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
     echo "::debug::开始上传到webdav"
     curl -X MKCOL -u $WEBDAV_USER:$WEBDAV_PASS $WEBDAV_URL
     curl -u $WEBDAV_USER:$WEBDAV_PASS -T $1 $WEBDAV_URL
