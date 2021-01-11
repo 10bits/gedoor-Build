a91f54b611a89fd3777e4241741ea4d81d08e96f
10bits <loseher2020@outlook.com>
2021-01-11 07:21:43 -0600

    update

--- action_upload.sh
+++ action_upload.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 #上传到webdav
-if [ $WEBDAV_UPLOAD = 'true' ] && [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
-  echo "::debug::开始上传到webdav"
-  curl -X MKCOL -u $WEBDAV_USER:$WEBDAV_PASS $WEBDAV_URL
-  curl -u $WEBDAV_USER:$WEBDAV_PASS -T $APP_UPLOAD/*.apk $WEBDAV_URL
-fi
+function webdav_upload()
+{
+  if [ -f $1 ] && [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
+    echo "::debug::开始上传到webdav"
+    curl -X MKCOL -u $WEBDAV_USER:$WEBDAV_PASS $WEBDAV_URL
+    curl -u $WEBDAV_USER:$WEBDAV_PASS -T $1 $WEBDAV_URL
+  fi
+}
