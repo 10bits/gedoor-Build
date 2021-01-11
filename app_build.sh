1954b33e2876bcfb2f9d734746f9e98614b4db70
10bits <loseher2020@outlook.com>
2021-01-11 07:42:44 -0600

    update

---
+++ app_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+function debug(){ echo "::debug::$1"; }
+
+#开始构建
+function app_build()
+{
+    debug "build with gradle"
+    cd $APP_WORKSPACE
+    chmod +x gradlew
+    ./gradlew assembleAppRelease
+    APP_BUILD_APK=$(find $APP_WORKSPACE/app/build -regex .*/release/.*.apk)
+    debug "build apk $APP_BUILD_APK"
+    if [ -f $APP_BUILD_APK ]; then
+        set_env APP_UPLOAD ${APP_BUILD_APK%/*}
+        debug "upload apk dir ${APP_BUILD_APK%/*}"
+    fi
+}
