81c12c86b3541e853e12dce294e92663eaa72c1c
10bits <loseher2020@outlook.com>
2021-01-11 16:19:28 +0800

    update

--- action_upload.sh
+++ action_upload.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #上传到webdav
-if [ $UPLOAD = 'true '] && [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
+if [ $UPLOAD = 'true' ] && [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
+  echo "::debug::开始上传到webdav"
   curl -X MKCOL -u $WEBDAV_USER:$WEBDAV_PASS $WEBDAV_URL
-  curl -u $WEBDAV_USER:$WEBDAV_PASS -T $WEBDAV_UPLOAD $WEBDAV_URL
+  curl -u $WEBDAV_USER:$WEBDAV_PASS -T $APP_UPLOAD $WEBDAV_URL
 fi
