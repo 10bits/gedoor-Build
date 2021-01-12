f8244cfe5eaa28d1c1fdd8881d48fee97a41e9e6
10bits <loseher2020@outlook.com>
2021-01-12 10:41:49 -0600

    update

--- action_upload.sh
+++ action_upload.sh
@@ -2,11 +2,10 @@
 
 function debug() { echo "::debug::$1"; }
 
-#上传到webdav
 function webdav_upload()
 {
   if [ $WEBDAV_UPLOAD = 'true' ] && [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ] && [ -n "$WEBDAV_URL" ] && [ $REPO_OWNER = $REPO_ACTOR ]; then
-    debug "开始上传到webdav"
+    debug "已开启webdav上传"
     curl -X MKCOL -u $WEBDAV_USER:$WEBDAV_PASS $WEBDAV_URL
     curl -u $WEBDAV_USER:$WEBDAV_PASS -T $1 $WEBDAV_URL
   fi
@@ -15,23 +14,23 @@ function webdav_upload()
 #TODO
 function http_upload()
 {
-  debug "上传到http服务器"
+  debug "已开启http上传"
 }
 
 #TODO
 function ftp_upload()
 {
-  debug "上传到ftp服务器"
+  debug "已开启ftp上传"
 }
 
 #TODO
 function sftp_upload()
 {
-  debug "上传到sftp服务器"
+  debug "已开启sftp上传"
 }
 
 #TODO
 function baiduyun_upload()
 {
- debug "上传到百度云盘"
+  debug "已开启百度网盘上传"
 }
