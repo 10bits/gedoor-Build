60706ebb76ae6c377dc479605a881eee7dfbf1e5
10bits <loseher2020@outlook.com>
2021-01-13 22:12:03 -0600

    update

--- action_app_custom.sh
+++ action_app_custom.sh
@@ -33,6 +33,7 @@ function app_bugme()
 
         debug "发现书籍界面优化"
         sed -e "/error(it)/i\isLoading = false" \
+            -e "/ExploreShowActivity/i\import io.legado.app.utils.*" \
             -e '/error(it)/i\toast(it)' \
             -e 's/error(it)/error("网络请求失败或超时")/' \
             $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt -i
