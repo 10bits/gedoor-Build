e331c8050a3eed8e18045b24bc8adba5d681ce91
10bits <loseher2020@outlook.com>
2021-01-15 08:26:45 -0600

    update

--- action_update.sh
+++ action_update.sh
@@ -4,7 +4,7 @@ function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 
 cd $GITHUB_WORKSPACE
 if version_gt $APP_LATEST_TAG $APP_LAST_TAG; then
-  sed "5c > 当前最新tag:$APP_LATEST_TAG 上次检查时间:$APP_LATEST_CHECK" README.md -i
+  sed "5c > 最新构建下载:[$APP_RELEASE_NAME]($APP_DOWNLOAD) 上次构建时间:$APP_LATEST_CHECK" README.md -i
   sed "1i $APP_LATEST_TAG" .lastcheck -i
   git config user.name  github-actions
   git config user.email github-actions@github.com
