53fa1592ea349205e43d73cc2a57479c3838c8ab
10bits <loseher2020@outlook.com>
2021-01-15 05:14:37 -0600

    update

--- action_app_custom.sh
+++ action_app_custom.sh
@@ -24,45 +24,6 @@ function app_rename()
     fi
 }
 
-#满足本人的一些小优化
-function app_bugme()
-{
-    if [ $APP_NAME = 'legado' ] && [[ $REPO_ACTOR = '10bits' ]]; then 
-        debug "maven中心仓库回归"
-        sed "/google()/i\        mavenCentral()" $APP_WORKSPACE/build.gradle -i
-
-        debug "发现书籍界面优化"
-        sed -e "/loadMoreView.error(it)/i\isLoading = false" \
-            -e "/ExploreShowActivity/i\import org.jetbrains.anko.toast" \
-            -e '/loadMoreView.error(it)/i\toast(it)' \
-            -e 's/loadMoreView.error(it)/loadMoreView.error("网络请求失败或超时")/' \
-            $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt -i
-        sed "s/30000L/6000L/" $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/explore/ExploreShowViewModel.kt -i
-        
-        debug "关闭加入书架提示"
-        START=$(sed -n '/!ReadBook.inBookshelf/=' $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt)
-        sed -e "$(echo $START+1|bc),$(echo $START+8|bc)d" \
-            -e '/!ReadBook.inBookshelf/a\viewModel.removeFromBookshelf{ super.finish() }' \
-            $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt -i
-        
-        debug "发现界面支持搜索书籍"
-        sed -e 's/getString(R.string.screen_find)/"搜索书籍、书源"/' \
-            -e '/fun initSearchView()/i\override fun onResume(){super.onResume();searchView.clearFocus()}' \
-            -e '/ExploreFragment/i\import io.legado.app.ui.book.search.SearchActivity' \
-            -e '/onQueryTextSubmit/a\if(!query?.contains("group:")!!){startActivity<SearchActivity>(Pair("key", query))}' \
-            $APP_WORKSPACE/app/src/main/java/io/legado/app/ui/main/explore/ExploreFragment.kt -i
-    fi
-}
-
-#软件内一些颜色调整,默认不启用
-function app_color_set()
-{
-    if [ $APP_NAME = 'legado' ]; then
-        debug "设置搜索界面浮动按钮颜色"
-        sed '/id\/fb_stop/a\        android:backgroundTint="#389099"' $APP_WORKSPACE/app/src/main/res/layout/activity_book_search.xml -i
-    fi
-}
-
 #删除一些用不到的资源
 function app_resources_unuse()
 {
@@ -134,7 +95,11 @@ fi
 
 #通用
 app_sign
+
 #阅读3.0
-app_bugme;app_clear_18plus;app_rename;app_live_together
+app_clear_18plus;app_rename;app_live_together
+#自用定制脚本
+source $APP_WORKSPACE/action_legado_myself.sh
+
 #阅读2.0
 app_not_apply_plugin
