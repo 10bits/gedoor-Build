c428d1785eba6923daa8624c672929d8d9a2d0ff
10bits <loseher2020@outlook.com>
2021-01-18 10:27:59 -0600

    update

--- action_app_build.sh
+++ action_app_build.sh
@@ -9,7 +9,7 @@ function app_build()
     debug "build with gradle"
     cd $APP_WORKSPACE
     chmod +x gradlew
-    ./gradlew assembleAppRelease --parallel
+    ./gradlew assembleAppRelease --build-cache --parallel
     APP_BUILD_APK=$(find $APP_WORKSPACE/app/build -regex .*/release/.*.apk)
     debug "build apk $APP_BUILD_APK"
     if [ -f $APP_BUILD_APK ]; then
