c5b8bf1d98f853bf9e36a3830ac837e5f4df8138
10bits <loseher2020@outlook.com>
2021-02-10 15:08:12 +0800

    版本号比较

--- action_util.sh
+++ action_util.sh
@@ -3,3 +3,6 @@
 function set_env() { echo "$1=$2" >> $GITHUB_ENV; }
 function debug() { echo "::debug::$1"; }
 function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
