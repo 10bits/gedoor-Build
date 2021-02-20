30b4f43e2dd9894ba2fba3ca3dfe50e6928ce8ff
10bits <loseher2020@outlook.com>
2021-02-19 18:11:42 -0600

    update

--- action_util.sh
+++ action_util.sh
@@ -10,8 +10,16 @@ function update_info()
 {
     cd $GITHUB_WORKSPACE
     if version_gt $APP_LATEST_TAG $APP_LAST_TAG; then
-        sed "5c > 最新构建下载:[$APP_RELEASE_NAME]($APP_DOWNLOAD) 上次构建时间:$APP_LATEST_CHECK" README.md -i
+        sed -e "s/^/> &/" \
+            -e "1i\<!--start-->" \
+            -e "$a\<!--end-->" \
+            $APP_LATEST_BODY -i
+        sed -e "/<!--start-->/<!--end-->/d" \
+            -e '5r '"$APP_LATEST_BODY"'' \
+            -e "5c > 最新构建下载:[$APP_RELEASE_NAME]($APP_DOWNLOAD) 上次构建时间:$APP_LATEST_CHECK" \
+            README.md -i
         sed "1i $APP_LATEST_TAG" .lastcheck -i
+ 
         git config user.name  github-actions
         git config user.email github-actions@github.com
         git commit -a -m "$APP_NAME-$APP_LATEST_TAG release"
