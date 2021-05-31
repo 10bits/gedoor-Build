cd6245fa23b0197eac53baf2b621156b92c6d3e5
10bits <loseher2020@outlook.com>
2021-05-31 09:14:49 -0500

    update

--- action_legado_myself.sh
+++ action_legado_myself.sh
@@ -78,7 +78,7 @@ function no_google_services()
 function my_launcher_icon(){
     debug "替换图标"
     find $APP_WORKSPACE/app/src -type d -regex '.*/res/drawable' -exec \
-    cp $GITHUB_WORKSPACE/.github/legado/ic_launcher_my.xml {}/ic_launcher2.xml \;
+    cp $GITHUB_WORKSPACE/.github/legado/ic_launcher_my.xml {}/ic_launcher1.xml \;
 }
 
 if [[ "$APP_NAME" == "legado" ]] && [[ "$REPO_ACTOR" == "10bits" ]]; then
