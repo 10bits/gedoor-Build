dd0dd26fa5873b35d81721378c5074a04646a3c9
10bits <loseher2020@outlook.com>
2021-08-11 09:26:06 +0800

    update

--- action_app_build.sh
+++ action_app_build.sh
@@ -13,11 +13,16 @@ function app_build()
         
     ./gradlew assembleAppRelease --build-cache --parallel
     
-    APP_BUILD_APK=$(find $APP_WORKSPACE/app/build -regex .*/release/.*.apk)
+    APP_BUILD_APK=$(find $APP_WORKSPACE/app/build -regex .*/app/release/.*.apk)
     debug "build apk $APP_BUILD_APK"
     if [ -f $APP_BUILD_APK ]; then
         set_env APP_BUILD_APK $APP_BUILD_APK
         set_env APP_UPLOAD ${APP_BUILD_APK%/*}
         debug "upload apk dir ${APP_BUILD_APK%/*}"
+    fi
+
+    if [[ "$APP_NAME" == "legado" ]]; then
+        ./gradlew assembleCronetRelease
+        set_env CRONET_BUILD_APK $(find $APP_WORKSPACE/app/build -regex .*/cronet/release/.*.apk)
     fi
 }
