6fd1d5ea78316116a5f17dd142bdfb560e313caa
10bits <loseher2020@outlook.com>
2023-06-04 21:14:33 +0800

    update

--- action_setenv.sh
+++ action_setenv.sh
@@ -5,7 +5,7 @@ function set_env() { echo "$1=$2" >> $GITHUB_ENV; }
 APP_NAME="legado"
 APP_GIT_URL="https://github.com/gedoor/legado.git"
 APP_SUFFIX="A"
-
+APP_CHANNEL="App"
 if [ $SECRETS_ENABLE = 'true' ] && [ -n "$SECRETS_APP_NAME" ] && [ -n "$SECRETS_GIT_URL" ] && [ $REPO_ACTOR = $REPO_OWNER ]; then
     APP_NAME=$SECRETS_APP_NAME
     APP_GIT_URL=$SECRETS_GIT_URL
