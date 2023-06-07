ef1b15613fe5eb3f53a7fb2d9071085ca59e63de
10bits <loseher2020@outlook.com>
2023-06-07 11:24:04 +0800

    update

--- diy/diy_10bits.sh
+++ diy/diy_10bits.sh
@@ -8,4 +8,13 @@ function my_launcher_icon(){
     find $APP_WORKSPACE/app/src -regex '.*/res/.*/ic_launcher.xml' -exec \
     sed "/background/d" {} -i \;
 }
+function bookshelfAdd_no_alert()
+{
+    debug "关闭加入书架提示"
+    find $APP_WORKSPACE/app/src -regex '.*/ReadBookActivity.kt' -exec \
+    sed -e '/fun finish()/,/fun onDestroy()/{s/alert/\/*&/;s/show()/&*\//}' \
+        -e '/!ReadBook.inBookshelf/a\viewModel.removeFromBookshelf{ super.finish() }' \
+        {} -i \;
+}
 my_launcher_icon;
+bookshelfAdd_no_alert;
